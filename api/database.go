7784be427e364882f1b5807d25b5ba1e08804c09
ShunsukeNaka <s.nak81.0510@gmail.com>
2026-02-12 11:17:13 +0900

    add Database Connect

---
+++ api/database.go
@@ -0,0 +1,48 @@
+// Based on https://neon.com/guides/golang-gorm-postgres
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	//Load Environment
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Connection string for Neon Postgres
+	dsn := os.Getenv("NEON_DB_URL")
+
+	// Connect to the database
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Get the underlying SQL DB object
+	sqlDB, err := DB.DB()
+	if err != nil {
+		log.Fatalf("Failed to get DB object: %v", err)
+	}
+
+	// Verify connection
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatalf("Failed to ping DB: %v", err)
+	}
+
+	fmt.Println("Successfully connected to Neon Postgres database!")
+}
