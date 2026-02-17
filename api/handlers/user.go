71b553c8f6d4825e1b443c3abf87322a422a913c
ShunsukeNaka <s.nak81.0510@gmail.com>
2026-02-17 13:28:06 +0900

    [change] トークンの確認の有無はhandlerの外で行う

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -2,67 +2,14 @@ package handlers
 
 import (
 	"api/models"
-	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func ParseToken(tokenString string) (uuid.UUID, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// 署名方法の確認
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method")
-		}
-		return []byte(os.Getenv("JWT_SECRET")), nil
-	})
-
-	if err != nil || !token.Valid {
-		return uuid.Nil, err
-	}
-
-	// 取り出す情報を指定
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		userIDStr := claims["user_id"].(string)
-		return uuid.Parse(userIDStr)
-	}
-
-	return uuid.Nil, fmt.Errorf("invalid token claims")
-}
-
-// =================ミドルウェアの定義==========================
-func AuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "認証トークンが必要です"})
-			c.Abort()
-			return
-		}
-
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-
-		userID, err := ParseToken(tokenString)
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "無効なトークンです"})
-			c.Abort()
-			return
-		}
-
-		// 3. コンテキスト(c)にユーザーIDをセットしておく
-		// これで、後のハンドラ関数で userID を取り出せるようになる
-		c.Set("userID", userID)
-		c.Next()
-	}
-}
-
-//=================ミドルウェアの定義=↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑=
 
 func CreateUser(c *gin.Context) {
 	type LoginRequest struct {
@@ -127,6 +74,10 @@ func Login(c *gin.Context) {
 		"token": tokenString,
 		"user":  user,
 	})
+}
+
+func GetMyProfile(c *gin.Context) {
+	id, exists := c.Get("user")
 }
 
 func GetUsers(c *gin.Context) {
--- api/main.go
+++ api/main.go
@@ -5,8 +5,13 @@ import (
 	"api/models"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -40,11 +45,20 @@ func main() {
 		})
 	})
 
+	// トークンをチェックしない
 	userRoutes := r.Group("/users")
 	{
 		userRoutes.POST("/", handlers.CreateUser)
 		userRoutes.GET("/", handlers.GetUsers)
 	}
+
+	//　以下はトークンをチェックする
+	protectedUserRoutes := r.Group("/users")
+	protectedUserRoutes.Use(AuthMiddleware())
+	{
+		protectedUserRoutes.GET("/getme", handlers.GetMyProfile)
+	}
+
 	// articleRoutes := r.Group("/article")
 	// {
 	// }
@@ -61,3 +75,53 @@ func main() {
 	// 8080ポートでサーバーを起動
 	r.Run(":8080")
 }
+
+func ParseToken(tokenString string) (uuid.UUID, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// 署名方法の確認
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return uuid.Nil, err
+	}
+
+	// 取り出す情報を指定
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		userIDStr := claims["user_id"].(string)
+		return uuid.Parse(userIDStr)
+	}
+
+	return uuid.Nil, fmt.Errorf("invalid token claims")
+}
+
+// =================ミドルウェアの定義==========================
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "認証トークンが必要です"})
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		userID, err := ParseToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "無効なトークンです"})
+			c.Abort()
+			return
+		}
+
+		// 3. コンテキスト(c)にユーザーIDをセットしておく
+		// これで、後のハンドラ関数で userID を取り出せるようになる
+		c.Set("userID", userID)
+		c.Next()
+	}
+}
+
+//=================ミドルウェアの定義=↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑=
